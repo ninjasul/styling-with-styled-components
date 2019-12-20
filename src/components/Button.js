2d4b2c82c6a94968468f169e4e51f7a59cc9ebfe
ninjasul <ninjasul@gmail.com>
2019-12-21 00:45:08 +0900

    버튼 사이즈 조정하기

--- src/components/Button.js
+++ src/components/Button.js
@@ -17,6 +17,28 @@ const colorStyles = css`
     }} 
 `;
 
+const sizes = {
+    large: {
+        height: '3rem',
+        fontSize: '1.25rem'
+    },
+    medium: {
+        height: '2.25rem',
+        fontSize: '1rem'
+    },
+    small: {
+        height: '1.75rem',
+        fontSize: '0.875rem'
+    }
+};
+
+const sizeStyles = css`
+    ${({ size }) => css`
+      height: ${sizes[size].height};
+      font-size: ${sizes[size].fontSize};
+    `}
+`;
+
 const StyledButton = styled.button`
     display: inline-flex;
     outline: none;
@@ -29,8 +51,7 @@ const StyledButton = styled.button`
     padding-right: 1rem;
     
     // 크기
-    height: 2.25rem;
-    font-size: 1rem;
+    ${sizeStyles}
     
     // 색상
     ${colorStyles}
@@ -41,16 +62,17 @@ const StyledButton = styled.button`
     }
 `
 
-function Button({children, color, ...rest}) {
+function Button({children, color, size, ...rest}) {
     return (
-        <StyledButton color={color} {...rest}>
+        <StyledButton color={color} size={size} {...rest}>
             {children}
         </StyledButton>
     );
 }
 
 Button.defaultProps = {
-    color: 'blue'
+    color: 'blue',
+    size: 'medium'
 };
 
 export default Button;
