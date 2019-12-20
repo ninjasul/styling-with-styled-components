3d86ca3f5bd8494ead059e38c3d7f62f711161d3
ninjasul <ninjasul@gmail.com>
2019-12-21 00:28:55 +0900

    polished 사용

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+import {darken, lighten} from 'polished';
+
+const colorStyles = css`
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+          background: ${selected};
+          &:hover {
+            background: ${lighten(0.1, selected)};
+          }
+          &:active {
+            background: ${darken(0.1, selected)};
+          }
+        `;
+    }} 
+`;
 
 const StyledButton = styled.button`
     display: inline-flex;
@@ -17,13 +33,7 @@ const StyledButton = styled.button`
     font-size: 1rem;
     
     // 색상
-    background: #228be6;
-    &:hover {
-      background: #339af0;
-    }
-    &:active {
-      background: #1c7ed6;
-    }
+    ${colorStyles}
     
     // 기타
     & + & {
@@ -31,8 +41,16 @@ const StyledButton = styled.button`
     }
 `
 
-function Button({ children, ...rest }) {
-    return <StyledButton {...rest}>{children}</StyledButton>;
+function Button({children, color, ...rest}) {
+    return (
+        <StyledButton color={color} {...rest}>
+            {children}
+        </StyledButton>
+    );
 }
+
+Button.defaultProps = {
+    color: 'blue'
+};
 
 export default Button;
