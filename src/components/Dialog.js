57e27ca6447d4a0b199a5ecfbea432e9d5e1f881
ninjasul <ninjasul@gmail.com>
2019-12-22 22:44:35 +0900

    Dialog 애니메이션 추가하기

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,6 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 import Button from './Button';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(200px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+`;
+
+const slideDown = keyframes`
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(200px);
+  }
+`;
 
 const DarkBackground = styled.div`
   position: fixed;
@@ -12,6 +48,18 @@ const DarkBackground = styled.div`
   align-items: center;
   justify-content: center;
   background: rgba(0, 0, 0, 0.8);
+  
+  animation-duration: 0.25s;
+  // 처음에는 빨랐다가 나중에는 느려짐.
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+  
+  ${props => 
+    props.disappear && 
+    css`
+      animation-name: ${fadeOut};
+    `}
 `;
 
 const DialogBlock = styled.div`
@@ -28,6 +76,18 @@ const DialogBlock = styled.div`
   p {
     font-size: 1.125rem;
   }
+  
+  animation-duration: 0.25s;
+  // 처음에는 빨랐다가 나중에는 느려짐.
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+  
+  ${props =>
+    props.disappear &&
+    css`
+      animation-name: ${slideDown};
+    `}
 `;
 
 const ButtonGroup = styled.div`
@@ -51,11 +111,23 @@ const ShortMarginButton = styled(Button)`
 `;
 
 function Dialog({title, children, confirmText, cancelText, onConfirm, onCancel, visible}) {
-    if (!visible) return null;
+    const [animate, setAnimate] = useState(false);
+    const [localVisible, setLocalVisible] = useState(visible);
+
+    useEffect(() => {
+        // visible true -> false
+        if (localVisible && !visible) {
+            setAnimate(true);
+            setTimeout(() => setAnimate(false), 250);
+        }
+        setLocalVisible(visible);
+    }, [localVisible, visible]);
+
+    if (!localVisible && !animate) return null;
 
     return (
-        <DarkBackground>
-            <DialogBlock>
+        <DarkBackground disappear={!visible}>
+            <DialogBlock disappear={!visible}>
                 <h3>{title}</h3>
                 <p>{children}</p>
                 <ButtonGroup>
